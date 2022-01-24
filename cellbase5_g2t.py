a60d02973a48cd135675b492bedfb39f2610f4b0
AishaD <AishaDahir75@gmail.com>
2022-01-24 12:43:05 +0000

    pr corrections pt1

--- cellbase5_g2t.py
+++ cellbase5_g2t.py
@@ -1,11 +1,10 @@
-from pandas.core.frame import DataFrame
-from pycellbase.cbconfig import ConfigClient
-from pycellbase.cbclient import CellBaseClient
-import pandas as pd
-from pandas.core.frame import DataFrame
 import argparse
 from datetime import date
+import pandas as pd
+from pycellbase.cbconfig import ConfigClient
+from pycellbase.cbclient import CellBaseClient
 
+# host contains the address for cellbase host
 from host import host_address
 
 
@@ -16,7 +15,7 @@ def parse_args():
     """
     
     parser = argparse.ArgumentParser(
-        description = 'HGNC tsv containg columns HGNC and ensemble gene ID')
+        description = 'HGNC tsv containg columns HGNC and ensembl gene ID')
     parser.add_argument(
         '-f', '--file',
         required=True,
@@ -27,53 +26,53 @@ def parse_args():
 
     return args
 
-def query_cellbase(gc, HGNC_df, HGNC_missing_ensembleID, ensembleID_not_in_cellbase, ensembleID_has_no_maneselect_refseq, all_genes):
+def query_cellbase(gc, HGNC_df, HGNC_missing_ensemblID, ensemblID_not_in_cellbase, ensemblID_has_no_maneselect_refseq, all_genes):
     """
     Inputs:
-        gc: cellbase gene client that is queried with a ensemble gene id
-        HGNC_df: dataframe containing HGNC id and ensemble gene id column 
-        HGNC_missing_ensembleID: empty list for HGNC ID that dont have an ensemble gene
-        ensembleID_not_in_cellbase: empty list for ensemble gene id not in cellbase
-        ensembleID_has_no_maneselect_refseq: empty list for ensemble genes that dont have maneselect_refseq
+        gc: cellbase gene client that is queried with a ensembl gene id
+        HGNC_df: dataframe containing HGNC id and ensembl gene id column 
+        HGNC_missing_ensemblID: empty list for HGNC ID that dont have an ensembl gene
+        ensemblID_not_in_cellbase: empty list for ensembl gene id not in cellbase
+        ensemblID_has_no_maneselect_refseq: empty list for ensembl genes that dont have maneselect_refseq
         all_genes: empty list that will hold HGNC id and their maneselect refseq transcript
     
     Returns:
         all_genes: list holding dictionaries of HGNC id and their maneselect refseq transcript
-        HGNC_missing_ensembleID: list for HGNC ID that dont have an ensemble gene
-        ensembleID_not_in_cellbase: list for ensemble gene id not in cellbase
-        ensembleID_has_no_maneselect_refseq: list for ensemble genes that dont have maneselect_refseq
+        HGNC_missing_ensemblID: list for HGNC ID that dont have an ensembl gene
+        ensemblID_not_in_cellbase: list for ensembl gene id not in cellbase
+        ensemblID_has_no_maneselect_refseq: list for ensembl genes that dont have maneselect_refseq
     """
     
     for row in range(len(HGNC_df)):
         HGNC_id = HGNC_df.loc[row, 'HGNC ID'] #HGNC ID is in 1st column
-        ensemble_id = HGNC_df.loc[row, 'Ensembl gene ID'] #Ensembl gene ID is in 12th column
-        # some genes do not have ensemble id so skip these
-        if pd.isna(ensemble_id):
-            print(HGNC_id + " does not have ensemble id to gene")
-            HGNC_missing_ensembleID.append(HGNC_id)
-            pass
+        ensembl_id = HGNC_df.loc[row, 'Ensembl gene ID'] #Ensembl gene ID is in 12th column
+        # some genes do not have ensembl id so skip these
+        if pd.isna(ensembl_id):
+            print(HGNC_id + " does not have ensembl id to gene")
+            HGNC_missing_ensemblID.append(HGNC_id)
+            continue
         else:
-            # make dict to keep info on what refseq is for ensemble gene in cellbase
+            # make dict to keep info on what refseq is for ensembl gene in cellbase
             gene_dict = {}
             gene_dict['HGNC_ID'] = HGNC_id
             # get all info and select transcript info
-            data = gc.get_info(ensemble_id)
-            # some ensemble gene id not present in cellbase so skip these
+            data = gc.get_info(ensembl_id)
+            # some ensembl gene id not present in cellbase so skip these
             if not data['responses'][0]['results']:
-                print(ensemble_id + " does not exist in cellbase5")
-                ensembleID_not_in_cellbase.append(ensemble_id)
+                print(f"{ensembl_id} does not exist in cellbase5")
+                ensemblID_not_in_cellbase.append(ensembl_id)
             else:
                 # how many transcripts are there?
                 txs_num = len(data['responses'][0]['results'][000]['transcripts'])
-                # for loop the ensemble transcripts to find what the refseq mane 
-                # transcripts are for the ensemble gene
+                # for loop the ensembl transcripts to find what the refseq mane 
+                # transcripts are for the ensembl gene
                 for transcript in range(txs_num):
                     dicts = list(data['responses'][0]['results'][0]['transcripts'][transcript]['xrefs'])
                     mane_dict = [item for item in dicts if item["dbName"] == "mane_select_refseq"]
                     if not mane_dict:
-                        # some ensemble transcripts do not have refseq mane transcript
+                        # some ensembl transcripts do not have refseq mane transcript
                         # for these, we skip to the next transcript
-                        ensembleID_has_no_maneselect_refseq.append(ensemble_id)
+                        ensemblID_has_no_maneselect_refseq.append(ensembl_id)
                         continue
                     else:
                         # refseq mane id is selected and added to the dict
@@ -81,7 +80,7 @@ def query_cellbase(gc, HGNC_df, HGNC_missing_ensembleID, ensembleID_not_in_cellb
                 # append the gene_dict to the list of all HGNC ids
                 all_genes.append(gene_dict) 
     
-    return all_genes, HGNC_missing_ensembleID, ensembleID_not_in_cellbase, ensembleID_has_no_maneselect_refseq
+    return all_genes, HGNC_missing_ensemblID, ensemblID_not_in_cellbase, ensemblID_has_no_maneselect_refseq
 
 
 
@@ -93,27 +92,25 @@ def main():
     ## Query cellbase5 database 
     custom_config = {'rest': {'hosts': [host_address]}, 
                     'version': 'v5', 'species': 'hsapiens'}
-    cc = ConfigClient(custom_config)
-    cbc = CellBaseClient(cc)
+    customconfigclient = ConfigClient(custom_config)
+    cbc = CellBaseClient(customconfigclient)
     cbc.show_configuration()['version']
     gc = cbc.get_gene_client() #select gene clients
 
     # read in the hgnc tsv from the cmd line
     args = parse_args()
-    input_filename = args.file
-
-    HGNC_df = pd.read_csv(input_filename, sep='\t')
+    HGNC_df = pd.read_csv(args.file, sep='\t')
     all_genes = []
 
-    # list of things that are missing for certain HGNC/ensemble IDs
-    HGNC_missing_ensembleID = []
-    ensembleID_not_in_cellbase = []
-    ensembleID_has_no_maneselect_refseq = []
+    # list of things that are missing for certain HGNC/ensembl IDs
+    HGNC_missing_ensemblID = []
+    ensemblID_not_in_cellbase = []
+    ensemblID_has_no_maneselect_refseq = []
     
-    # input the lists and get out the 
-    all_genes, HGNC_missing_ensembleID, ensembleID_not_in_cellbase, ensembleID_has_no_maneselect_refseq = query_cellbase(
-        gc, HGNC_df, HGNC_missing_ensembleID, ensembleID_not_in_cellbase, 
-        ensembleID_has_no_maneselect_refseq, all_genes)
+    # input the lists and get out the filled out lists
+    all_genes, HGNC_missing_ensemblID, ensemblID_not_in_cellbase, ensemblID_has_no_maneselect_refseq = query_cellbase(
+        gc, HGNC_df, HGNC_missing_ensemblID, ensemblID_not_in_cellbase, 
+        ensemblID_has_no_maneselect_refseq, all_genes)
 
     # Save all hgnc ids and relevent refseq mane transcripts to dataframe 
     df = pd.DataFrame.from_dict(all_genes)
@@ -122,34 +119,36 @@ def main():
     # rest index to allow merging of columns later
     df_noNaN = df_noNaN.reset_index(drop=True) 
 
-    #make two lists for canonical and clinical
-    clinical_list = ['clinical_transcript'] * len(df_noNaN)
-    canonical_list = ['canonical'] * len(df_noNaN)
-
-    #change into dataframe columns
-    clinical_list = pd.DataFrame({'clinical_list': clinical_list})
-    canonical_list = pd.DataFrame({'canonical_list': canonical_list})
-
-    #merge dataframe and columnc
-    complete_dataframe = pd.concat([df_noNaN, clinical_list,canonical_list], axis=1)
+    #Add two lists for canonical and clinical
+    df_noNaN['clinical_list'] = 'clinical_transcript'
+    df_noNaN['canonical_list'] = 'canonical'
 
     ##### save outputs
-    today = date.today()
+    today = date.today().strftime("%Y%m%d")
 
-    complete_dataframe_outputname = today.strftime("%Y%m%d") +'_g2t_b38.tsv'
-    df_outputname = today.strftime("%Y%m%d") +'_g2t_b38_all.tsv'
-    complete_dataframe.to_csv(complete_dataframe_outputname, sep="\t", header=False, index=False)
+    df_noNaN_outputname = today +'_g2t_b38.tsv'
+    df_outputname = today +'_g2t_b38_all.tsv'
+    df_noNaN.to_csv(df_noNaN_outputname, sep="\t", header=False, index=False)
     df.to_csv(df_outputname, sep="\t", header=False, index=False)
 
     #save list that wont be in the final file
     #change into dataframe columns
-    HGNC_missing_ensembleID = pd.DataFrame({'HGNC_missing_ensembleID': HGNC_missing_ensembleID})
-    ensembleID_not_in_cellbase = pd.DataFrame({'ensembleID_not_in_cellbase': ensembleID_not_in_cellbase})
-    ensembleID_has_no_maneselect_refseq = pd.DataFrame({'ensemble_gene_has_no_maneselect_refseq': ensembleID_has_no_maneselect_refseq})
+    HGNC_missing_ensemblID = pd.DataFrame({
+        'HGNC_missing_ensemblID': HGNC_missing_ensemblID
+        })
+    ensemblID_not_in_cellbase = pd.DataFrame({
+        'ensemblID_not_in_cellbase': ensemblID_not_in_cellbase
+        })
+    ensemblID_has_no_maneselect_refseq = pd.DataFrame({
+        'ensembl_gene_has_no_maneselect_refseq': ensemblID_has_no_maneselect_refseq
+        })
     #merge dataframe and column
-    missinginfo_dataframe = pd.concat([HGNC_missing_ensembleID, ensembleID_not_in_cellbase,ensembleID_has_no_maneselect_refseq], axis=1)
+    missinginfo_dataframe = pd.concat(
+        [HGNC_missing_ensemblID, ensemblID_not_in_cellbase,
+        ensemblID_has_no_maneselect_refseq],
+        axis=1)
 
-    missinginfo_dataframe_outputname = today.strftime("%Y%m%d") +'_g2t_b38_missing_info.tsv'
+    missinginfo_dataframe_outputname = today +'_g2t_b38_missing_info.tsv'
     missinginfo_dataframe.to_csv(missinginfo_dataframe_outputname, sep="\t", header=True, index=False)
 
 if __name__ == "__main__":
